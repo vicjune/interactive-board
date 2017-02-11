393cc09e6097ebca3829c5e3e151f62016c6bb8f
vicjune <victor.jouin@gmail.com>
2017-02-11 12:03:29 +0100

    [magnet] implement drag and drop

--- src/app/components/magnet/magnet.component.ts
+++ src/app/components/magnet/magnet.component.ts
@@ -15,29 +15,56 @@ export class MagnetComponent implements OnInit {
     ) {}
 
     coordinates: number[] = [0, 0];
-    @Input() private id: number;
+    mouseOffset: number[] = [0, 0];
+    status = {
+        drag: false,
+        ready: false
+    };
+    @Input() color: string;
+    @Input() id: number;
+
 
     ngOnInit () {
         this.FirebaseService.bindObject(this.id).subscribe(
             firebaseObject => {
                 this.updateCoordinates(firebaseObject);
+                this.status.ready = true;
             },
-            error => console.error(error),
-            () => {}
+            error => console.error(error)
         );
     }
 
+    mouseDown(e) {
+        this.status.drag = true;
+        this.mouseOffset = [e.layerX, e.layerY];
+    }
+
+    mouseMove(e) {
+        if (this.status.drag) {
+            this.coordinates = [e.clientX - this.mouseOffset[0], e.clientY - this.mouseOffset[1]];
+        }
+    }
+
+    mouseUp() {
+        this.sendCoordinates();
+    }
+
     private updateCoordinates(firebaseObject: any) {
-        this.coordinates[0] = firebaseObject.x;
-        this.coordinates[1] = firebaseObject.y;
+        if (!this.status.drag) {
+            this.coordinates[0] = firebaseObject.x;
+            this.coordinates[1] = firebaseObject.y;
+        }
     }
 
     private sendCoordinates() {
         this.FirebaseService.setCoordinates(this.id, this.coordinates).subscribe(
             null,
-            error => console.error(error),
+            error => {
+                console.error(error);
+                this.status.drag = false;
+            },
             () => {
-                console.log('ok');
+                this.status.drag = false;
             }
         );
     }
