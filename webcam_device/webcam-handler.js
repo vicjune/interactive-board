bd5491b4e378f022ac1671b23229e150a28d33b1
vicjune <victor@ileotech.com>
2017-03-17 12:17:20 +0100

    [webcam] test node

--- webcam_device/webcam-handler.js
+++ webcam_device/webcam-handler.js
@@ -1,2 +1,16 @@
-var FfmpegCommand = require('fluent-ffmpeg');
-var command = new FfmpegCommand();
+console.log('ok');
+var ffmpeg = require('fluent-ffmpeg');
+var command = new ffmpeg();
+
+ffmpeg('/dev/video0')
+    .inputFormat('video4linux2')
+    .inputFPS(25)
+    .output('http://192.168.1.138:8081/d1zqvdzz')
+    .format('mpegts')
+    .videoCodec('mpeg1video')
+    .size('640x480')
+    .on('start', function(commandLine) {
+        console.log('Spawned Ffmpeg with command: ' + commandLine);
+    });
+
+console.log('started');
