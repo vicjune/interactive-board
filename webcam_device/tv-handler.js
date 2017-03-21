06b7b25862ef61168363a001a05e6448b71c46a0
vicjune <victor@ileotech.com>
2017-03-21 17:18:13 +0100

    [webcam_device] add tv handler

--- webcam_device/tv-handler.js
+++ webcam_device/tv-handler.js
@@ -1,23 +1,32 @@
-let exec = require('child_process').exec;
-//firebase
+let exec = require('child_process').exec,
+    firebase = require('firebase');
 
-setInterval(() => {
+firebase.initializeApp({
+    apiKey: "AIzaSyCcqVCaKizJ-HvqQaHQ86HUlZpdGVGCWPU",
+    authDomain: "interactive-board-999c5.firebaseapp.com",
+    databaseURL: "https://interactive-board-999c5.firebaseio.com",
+    storageBucket: "interactive-board-999c5.appspot.com"
+});
+
+firebase.database().ref('/streamOpen').on('value', function(payload) {
+    streamOpen = payload.val() === true ? true : false;
+
     exec('echo "pow 0" | cec-client -s', (error, stdout, stderr) => {
         if (error === null) {
             let n = stdout.indexOf("power status:");
-            status_temp = stdout.substring(n+13, n+28);
-            status = status_temp.substring(0, status_temp.indexOf("D")-1);
-            if (status === 'on' && !openingHours) {
+            let status_temp = stdout.substring(n+13, n+28);
+            let status = status_temp.substring(0, status_temp.indexOf("D")-1);
+            if (status === 'on' && !streamOpen) {
                 turnTv('standby');
             }
-            if (status === 'standby' && openingHours) {
+            if (status === 'standby' && streamOpen) {
                 turnTv('on');
             }
         } else {
             console.log('exec error: ' + error);
         }
     });
-}, 60000);
+});
 
 function turnTv (newStatus) {
     exec('echo "' + newStatus + ' 0" | cec-client -s', (error, stdout, stderr) => {
