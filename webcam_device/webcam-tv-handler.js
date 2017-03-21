61a3c69393768685b24f907b1c7c3fd66507e159
vicjune <victor@ileotech.com>
2017-03-21 18:08:49 +0100

    [webcam_device] add node webcam

---
+++ webcam_device/webcam-tv-handler.js
@@ -0,0 +1,66 @@
+// REQUIRED FILE: webcam-secret.json - Content example: "secret"
+
+let exec = require('child_process').exec;
+let streamSecret = require('./webcam-secret.json');
+let firebase = require('firebase');
+
+firebase.initializeApp({
+    apiKey: "AIzaSyCcqVCaKizJ-HvqQaHQ86HUlZpdGVGCWPU",
+    authDomain: "interactive-board-999c5.firebaseapp.com",
+    databaseURL: "https://interactive-board-999c5.firebaseio.com",
+    storageBucket: "interactive-board-999c5.appspot.com"
+});
+
+let webcamExec;
+
+firebase.database().ref('/server').on('value', server => {
+    firebase.database().ref('/streamOpen').on('value', payload => {
+        streamOpen = payload.val() === true ? true : false;
+
+        exec('echo "pow 0" | cec-client -s', (error, stdout, stderr) => {
+            if (error === null) {
+                let n = stdout.indexOf("power status:");
+                let status_temp = stdout.substring(n+13, n+28);
+                let status = status_temp.substring(0, status_temp.indexOf("D")-1).trim();
+                if (status === 'on' && !streamOpen) {
+                    turnTv('standby');
+                }
+                if (status === 'standby' && streamOpen) {
+                    turnTv('on');
+                }
+            } else {
+                console.log('exec error: ' + error);
+            }
+        });
+
+        if (streamOpen) {
+            webcamExec = exec('ffmpeg -r 25 -f video4linux2 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 ' + server.val().ip + ':' + server.val().streamPort + '/' + streamSecret + ' > /home/pi/Projects/interactive-board/webcam_device/webcam.log', (error, stdout, stderr) => {
+                if (error === null) {
+                    console.log('ok');
+                } else {
+                    console.log('exec error: ' + error);
+                }
+            });
+        } else {
+            if (webcamExec) {
+                webcamExec.kill();
+            }
+        }
+    });
+});
+
+function turnTv (newStatus) {
+    exec('echo "' + newStatus + ' 0" | cec-client -s', (error, stdout, stderr) => {
+        if (error === null) {
+            if (newStatus === 'on') {
+                exec('echo "as" | cec-client -s', (error, stdout, stderr) => {
+                    if (error !== null) {
+                        console.log('exec error: ' + error);
+                    }
+                });
+            }
+        } else {
+            console.log('exec error: ' + error);
+        }
+    });
+}
