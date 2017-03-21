76342cb0f24b528aebdae9948c8088d61484d810
vicjune <victor@ileotech.com>
2017-03-21 18:11:44 +0100

    [webcam_device] fix webcam

--- webcam_device/webcam-tv-handler.js
+++ webcam_device/webcam-tv-handler.js
@@ -34,7 +34,7 @@ firebase.database().ref('/server').on('value', server => {
         });
 
         if (streamOpen) {
-            webcamExec = exec('ffmpeg -r 25 -f video4linux2 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 ' + server.val().ip + ':' + server.val().streamPort + '/' + streamSecret + ' > /home/pi/Projects/interactive-board/webcam_device/webcam.log', (error, stdout, stderr) => {
+            webcamExec = exec('ffmpeg -r 25 -f video4linux2 -i /dev/video0 -f mpegts -codec:v mpeg1video -s 640x480 http://' + server.val().ip + ':' + server.val().streamPort + '/' + streamSecret + ' > /home/pi/Projects/interactive-board/webcam_device/webcam.log', (error, stdout, stderr) => {
                 if (error === null) {
                     console.log('ok');
                 } else {
